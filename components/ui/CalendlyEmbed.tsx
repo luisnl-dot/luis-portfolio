c9f71747f98c3d89cd49076dfef31e98b5d0a9fb
Luis Nagel <luisnagel@Air-von-Luis.fritz.box>
2026-03-26 00:04:14 +0100

    Fix Calendly embed type

--- components/ui/CalendlyEmbed.tsx
+++ components/ui/CalendlyEmbed.tsx
@@ -1,7 +1,7 @@
 export default function CalendlyEmbed() {
   return (
     <iframe
-      src="https://calendly.com/luisn-l/30min?hide_landing_page_details=1&hide_gdpr_banner=1&background_color=141414&text_color=ffffff&primary_color=39FFB0"
+      src="https://calendly.com/luisn-l/30min?embed_type=Inline&hide_landing_page_details=1&hide_gdpr_banner=1&background_color=141414&text_color=ffffff&primary_color=39FFB0"
       width="100%"
       height="900"
       frameBorder="0"
